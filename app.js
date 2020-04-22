69ac61c3dedf27b69fca1586c2d7ebd3a21f2913
jayesh.sathe <jayesh.sathe@gmail.com>
2020-04-21 20:29:58 -0700

    modifying file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const Pizza = require('./Pizza') ;
 const ToppingDecorator = require('./ToppingDecorator') ;
+console.log("TESTING JENKINS");
 var piz = new Pizza("Margherita", 25);
     var price = piz.getPrice();
 	console.log(" PRICE => " + price);
  
-    var decorated = new ToppingDecorator(piz, 10);
+    var decorated = new ToppingDecorator(piz, 45);
     price = decorated.getPrice();
 	console.log(" NEW PRICE => " + price);
